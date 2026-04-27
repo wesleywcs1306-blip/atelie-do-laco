8b5fab5363a31376b85edff1a973758c14d4a90e
wesleywcs1306-blip <wesley.wcs1306@gmail.com>
2026-04-26 22:25:12 -0300

    Update sw.js

--- admin/sw.js
+++ admin/sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'atelie-admin-v6';
 const ASSETS = ['./index.html', './manifest.json'];
-
+ 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
+ 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -14,9 +14,10 @@ self.addEventListener('activate', e => {
   );
   self.clients.claim();
 });
-
+ 
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
   );
 });
+ 
